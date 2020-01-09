9e55f85b88da654d4a7eba759f988e75993334f7
dwr <damian.wrobel.1990@gmail.com>
2020-01-09 21:04:00 +0100

    klasa Driver

---
+++ src/main/java/sdacademy/demo/entity/Driver.java
@@ -0,0 +1,74 @@
+package sdacademy.demo.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "driver")
+public class Driver {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "driver")
+    private DriverDetail driverDetail;
+
+    public Driver() {
+    }
+
+    public Driver(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public DriverDetail getDriverDetail() {
+        return driverDetail;
+    }
+
+    public void setDriverDetail(DriverDetail driverDetail) {
+        this.driverDetail = driverDetail;
+    }
+
+    @Override
+    public String toString() {
+        return "Driver{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", driverDetail=" + driverDetail +
+                '}';
+    }
+}
